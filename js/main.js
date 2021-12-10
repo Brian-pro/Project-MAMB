4bcda0f0c9bdebff735ecf570a700b13396394db
Brian Rizer <55166349+Brian-pro@users.noreply.github.com>
2021-12-10 14:01:54 -0500

    things are broken but pushing so I can work on other computer

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 /* Selectors */
-document.querySelector('.login-btn');
+const loginButton = document.querySelector('.navbar-right-container');
+const closeLoginButton = document.querySelector('.close-login');
 
 /* Event listeners */
-const
+loginButton.addEventListener('click', showLoginPopup);
+closeLoginButton.addEventListener('click', closeLoginPopup);
 
 /* Functions */
+function showLoginPopup(event) {
+    document.querySelector('.login-background').classList.add('show');
+}
+
+function closeLoginPopup() {
+    document.querySelector('.login-background').style.display = 'none';
+}
