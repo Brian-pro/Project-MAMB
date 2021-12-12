bee7a8c7ed31b2b3a1098a86717b9122d490a0cd
Brian Rizer <55166349+Brian-pro@users.noreply.github.com>
2021-12-12 10:50:52 -0500

    Making the Login Popup modal looks way better (Not Finished)
    Slowly adding better styling!

--- js/main.js
+++ js/main.js
@@ -7,12 +7,20 @@ loginButton.addEventListener('click', showLoginPopup);
 closeLoginButton.addEventListener("click", closeLoginPopup);
 
 /* Functions */
+
+/* showLoginPopup
+    - Allows navbar login button to display login modal
+      popup. Done by changing from display: none -> display: flex
+*/
 function showLoginPopup(event) {
     event.preventDefault();
     document.querySelector('.login-background').setAttribute('style', 'display: flex');
 }
 
-
+/* closeLoginPopup
+    - Allows user exit out of login modal popup. Done
+      by changing display: flex -> display: none
+*/
 function closeLoginPopup(event) {
     event.preventDefault();
     document.querySelector('.login-background').setAttribute('style', 'display: none');
