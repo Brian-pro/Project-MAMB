bcd7cd8926790af7120234fbc92e9fba4b280a9f
Brian Rizer <55166349+Brian-pro@users.noreply.github.com>
2021-12-15 20:43:17 -0500

    Created Sign Up Popup and worked out button issues

--- js/main.js
+++ js/main.js
@@ -1,28 +1,47 @@
 /* Selectors */
 const loginButton = document.querySelector('.login-btn');
-const closeLoginButton = document.querySelector('.close-modal');
+const signupButton = document.querySelector('.signup-btn');
+const signupButton2 = document.querySelector('.signup-btn-2');
+const closeLoginButton = document.querySelector('.close-login-modal');
+const closeSignupButton = document.querySelector('.close-signup-modal');
 
 /* Event listeners */
-loginButton.addEventListener('click', showLoginPopup);
-closeLoginButton.addEventListener("click", closeLoginPopup);
+loginButton.addEventListener('click', showPopup);
+signupButton.addEventListener('click', showPopup);
+signupButton2.addEventListener('click', showPopup);
+closeLoginButton.addEventListener('click', closePopup);
+closeSignupButton.addEventListener('click', closePopup);
 
 /* Functions */
 
 /* showLoginPopup
     - Allows navbar login button to display login modal
       popup. Done by changing from display: none -> display: flex
+
+      This now also works for the sign up button as well!
 */
-function showLoginPopup(event) {
+function showPopup(event) {
     event.preventDefault();
-    console.log("Hello")
-    document.querySelector('.login-background').setAttribute('style', 'display: flex');
+    console.log(event.target.classList[0]);
+    if(event.target.classList[0] === 'login-btn') {
+        document.querySelector('.login-background').setAttribute('style', 'display: flex');
+    }
+    else {
+        document.querySelector('.signup-background').setAttribute('style', 'display: flex');
+    }  
 }
 
 /* closeLoginPopup
     - Allows user exit out of login modal popup. Done
       by changing display: flex -> display: none
 */
-function closeLoginPopup(event) {
+function closePopup(event) {
     event.preventDefault();
-    document.querySelector('.login-background').setAttribute('style', 'display: none');
+    console.log(event.target.classList[2]);
+    if (event.target.classList[2] === 'close-login-modal') {
+        document.querySelector('.login-background').setAttribute('style', 'display: none');
+    }
+    else {
+        document.querySelector('.signup-background').setAttribute('style', 'display: none');
+    }
 }
